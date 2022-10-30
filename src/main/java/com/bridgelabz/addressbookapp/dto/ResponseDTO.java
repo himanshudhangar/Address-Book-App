0100e84da83b8b1cfe2b15c4fafa0517108a2d2b
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-10-30 12:21:59 +0530

    Added:Used Lombok Library to Auto Generate Getter and Setter

--- src/main/java/com/bridgelabz/addressbookapp/dto/ResponseDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/ResponseDTO.java
@@ -4,11 +4,11 @@
 package com.bridgelabz.addressbookapp.dto;
 
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 public class ResponseDTO {
     private String message;
     private Object data;
