af91afd71c923fb5a7856233c932fa2f9d543569
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-10-30 10:32:02 +0530

    Added:Introduce DTO and Model in Address Book App

---
+++ src/main/java/com/bridgelabz/addressbookapp/dto/ResponseDTO.java
@@ -0,0 +1,20 @@
+/**
+ * ResponseDTO.java
+ */
+package com.bridgelabz.addressbookapp.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResponseDTO {
+    private String message;
+    private Object data;
+
+    public ResponseDTO(String message, Object data) {
+        this.message = message;
+        this.data = data;
+    }
+}
