388fc29465b6385c7515d952a659085964b0dd43
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-10-30 17:37:38 +0530

    Added:Throws(Custom Excepation)User Friendly Error in Case Addressbook App

---
+++ src/main/java/com/bridgelabz/addressbookapp/excepation/AddressbookException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbookapp.excepation;
+
+public class AddressbookException extends RuntimeException{
+    public AddressbookException(String message) {
+        super(message);
+    }
+}
