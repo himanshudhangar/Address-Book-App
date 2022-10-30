a426e5d84179d2a2295dce8c71167eecfcd09306
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-10-30 13:21:58 +0530

    Added:Set up The Dev and Prod Properties

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -1,8 +1,9 @@
-package com.bridgelabz.addressBookApp;
+package com.bridgelabz.addressbookapp;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
@@ -10,7 +11,10 @@ public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Address Book Application");
-		SpringApplication.run(AddressBookAppApplication.class, args);
+		ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
+
+		log.info("Addressbook App Started in {} Environment",
+				context.getEnvironment().getProperty("environment"));
 		log.info("Addressbook App started");
 	}
 
